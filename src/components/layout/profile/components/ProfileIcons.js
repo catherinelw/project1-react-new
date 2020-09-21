4616178d8cb58babf3aedd070c67304fad5efcf9
catherine <catherine@catherinedembp.home>
2020-09-21 15:43:30 +1000

    link to websites

--- src/components/layout/profile/components/ProfileIcons.js
+++ src/components/layout/profile/components/ProfileIcons.js
@@ -7,8 +7,12 @@ const profileIcons = () => {
 
         return (
             <div className={styles.iconsContainer}>
+            <a href="http://www.linkedin.com/in/lujiawen">
             <img src={linkedin} alt="linkedin" />
+            </a>
+            <a href="https://github.com/catherinelw">
             <img src={github} alt="github" />
+            </a>
             </div>
         )
     }
